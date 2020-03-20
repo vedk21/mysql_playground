06330af5ccb5b1d26c15c6a9105192ebc1b19274
vedk21 <vedantkakade.cp@gmail.com>
2020-03-21 00:20:35 +0530

    Added: basic database create and drop queries

---
+++ Basic/Database/database.sql
@@ -0,0 +1,7 @@
+-- Add Database query --
+-- CREATE DATABASE <NAME_OF_DATABASE> --
+CREATE DATABASE champions_league
+
+-- Drop Database query --
+-- DROP DATABASE <NAME_OF_DATABASE>
+DROP DATABASE champions_league
