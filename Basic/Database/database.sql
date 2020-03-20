e64259c29185c30cc2b0a6b907809e76c1b48668
vedk21 <vedantkakade.cp@gmail.com>
2020-03-21 00:28:16 +0530

    Added: use database and check current database queries

--- Basic/Database/database.sql
+++ Basic/Database/database.sql
@@ -1,7 +1,15 @@
--- Add Database query --
+-- Add database query --
 -- CREATE DATABASE <NAME_OF_DATABASE> --
-CREATE DATABASE champions_league
+CREATE DATABASE champions_league;
 
--- Drop Database query --
--- DROP DATABASE <NAME_OF_DATABASE>
-DROP DATABASE champions_league
+-- Drop database query --
+-- DROP DATABASE <NAME_OF_DATABASE> --
+DROP DATABASE champions_league;
+
+-- Use database query --
+-- USE <NAME_OF_DATABASE> --
+USE champions_league;
+
+-- Check for current database --
+-- SELECT database() --
+SELECT database();
