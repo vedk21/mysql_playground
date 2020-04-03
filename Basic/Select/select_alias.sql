e910e43c4f5b85bf4117803649d69165b70b9d3b
vedk21 <vedantkakade.cp@gmail.com>
2020-04-03 19:09:46 +0530

    Added: alias selection

---
+++ Basic/Select/select_alias.sql
@@ -0,0 +1,6 @@
+-- Use database --
+USE champions_league;
+
+-- Selecting values using aliases --
+-- SELECT <COLUMN_NAME> AS <ALIAS_NAME> FROM <NAME_OF_TABLE> --
+SELECT team_name AS name FROM teams;
