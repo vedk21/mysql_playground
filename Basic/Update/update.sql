3feb0ee4dfa7658361ac2e5105b216a689aaae4b
vedk21 <vedantkakade.cp@gmail.com>
2020-04-03 20:12:10 +0530

    Added: update table record query

---
+++ Basic/Update/update.sql
@@ -0,0 +1,6 @@
+-- Use database --
+USE champions_league;
+
+-- Update the record in the table --
+-- UPDATE <NAME_OF_TABLE> SET <COLUMN_NAME> = <VALUE> WHERE <COLUMN_NAME> = <VALUE> --
+UPDATE teams SET team_name = 'ASTON VILLA' WHERE team_id = 3;
