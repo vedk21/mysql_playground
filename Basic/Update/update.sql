56bc812a8a093d747c3473831fbf11714e8044fb
vedk21 <vedantkakade.cp@gmail.com>
2020-04-03 20:22:31 +0530

    Modified: update all records query added

--- Basic/Update/update.sql
+++ Basic/Update/update.sql
@@ -4,3 +4,7 @@ USE champions_league;
 -- Update the record in the table --
 -- UPDATE <NAME_OF_TABLE> SET <COLUMN_NAME> = <VALUE> WHERE <COLUMN_NAME> = <VALUE> --
 UPDATE teams SET team_name = 'ASTON VILLA' WHERE team_id = 3;
+
+-- Update all records in the table --
+-- UPDATE <NAME_OF_TABLE> SET <COLUMN_NAME> = <VALUE> --
+UPDATE teams SET country = 'SPAIN';
