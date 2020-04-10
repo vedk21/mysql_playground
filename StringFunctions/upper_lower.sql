c2429014585c12d7f594c09b5268425c9e8904a7
vedk21 <vedantkakade.cp@gmail.com>
2020-04-11 00:16:41 +0530

    Added: string functions: upper and lower query

---
+++ StringFunctions/upper_lower.sql
@@ -0,0 +1,9 @@
+-- Use database --
+USE champions_league;
+
+-- Convert string into uppercase or lowercase --
+-- SELECT UPPER(<string>) FROM <NAME_OF_TABLE>; --
+SELECT UPPER(country) FROM teams;
+
+-- SELECT LOWER(<string>) FROM <NAME_OF_TABLE>; --
+SELECT LOWER(country) FROM teams;
