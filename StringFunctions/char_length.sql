bae26907a39a9739a9300d94cda0d021a15be142
vedk21 <vedantkakade.cp@gmail.com>
2020-04-10 23:46:44 +0530

    Added: string functions: char_length query

---
+++ StringFunctions/char_length.sql
@@ -0,0 +1,6 @@
+-- Use database --
+USE champions_league;
+
+-- Count number of characters in the string --
+-- SELECT CHAR_LENGTH(<string>) FROM <NAME_OF_TABLE>; --
+SELECT CHAR_LENGTH(country) FROM teams;
