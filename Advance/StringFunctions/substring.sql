662cd4c862eca8bb16202edb4a789a53374f6989
vedk21 <vedantkakade.cp@gmail.com>
2020-04-11 23:40:16 +0530

    Updated: few corner conditions added in substring string functions

--- Advance/StringFunctions/substring.sql
+++ Advance/StringFunctions/substring.sql
@@ -8,3 +8,11 @@ SELECT SUBSTRING(team_name, 1, 5) FROM teams;
 -- SHORTCUT FOR SUBSTRING is SUBSTR
 -- SELECT SUBSTR(<string>, <start>, <end>) FROM <NAME_OF_TABLE>;
 SELECT SUBSTR(team_name, 1, 5) FROM teams;
+
+-- We can only pass starting point also, hence end will be by default end of the string
+-- SELECT SUBSTR(<string>, <start>) FROM <NAME_OF_TABLE>;
+SELECT SUBSTR(team_name, 3) FROM teams;
+
+-- We can only pass negative number as well, where -1 represents last char, -2 second last char and so on
+-- SELECT SUBSTR(<string>, [-<start>]) FROM <NAME_OF_TABLE>;
+SELECT SUBSTR(team_name, -3) FROM teams;
