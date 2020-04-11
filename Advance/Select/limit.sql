60457630b8343fa24e8d748feea4f2a52892c6f2
vedk21 <vedantkakade.cp@gmail.com>
2020-04-12 00:09:51 +0530

    Added: limit query

---
+++ Advance/Select/limit.sql
@@ -0,0 +1,10 @@
+-- Use database --
+USE champions_league;
+
+-- Limit the number of entries projected --
+-- SELECT <COLUMN_NAME1>, ..., <COLUMN_NAMEn> FROM <> LIMIT <limit_number>;
+SELECT team_name, country FROM teams LIMIT 2;
+
+-- Limit with a starting point --
+-- SELECT <COLUMN_NAME1>, ..., <COLUMN_NAMEn> FROM <> LIMIT <starting_limit_number>, <number_of_records>;
+SELECT team_name, country FROM teams LIMIT 1, 2;
