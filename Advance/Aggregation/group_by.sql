ea51cfbf97cb840e310eb84bf6dfeed74044679a
vedk21 <vedantkakade.cp@gmail.com>
2020-04-13 23:03:46 +0530

    Modified: Comment regarding group by clause added

--- Advance/Aggregation/group_by.sql
+++ Advance/Aggregation/group_by.sql
@@ -3,4 +3,5 @@ USE champions_league;
 
 -- Group records on a column data or columns data --
 -- SELECT <COLUMN_NAME>, <AGGREGATE_FUNCTION> FROM <NAME_OF_TABLE> GROUP BY <COLUMN_NAME>;
+-- We can only project those columns which used for grouping --
 SELECT country, COUNT(*) FROM teams GROUP BY country;
