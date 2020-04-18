d651455c7fcb25d1a38f23a8faffa59bd4a2750f
vedk21 <vedantkakade.cp@gmail.com>
2020-04-19 01:00:27 +0530

    Modified: foreign key - on delete cascade condition added

--- Pro/TablesRelationship/foreign_key.sql
+++ Pro/TablesRelationship/foreign_key.sql
@@ -32,3 +32,21 @@ CREATE TABLE players
   PRIMARY KEY (player_id),
   FOREIGN KEY (team_id) REFERENCES teams(team_id)
 );
+
+-- On delete cascade --
+-- If we delete the parent record then the child records dependent on it will also be deleted --
+-- If no on delete cascade is added on table, then it will throw an error if we delete such parent data --
+-- CREATE TABLE <NAME_OF_TABLE2>
+-- (
+--    column_name data_type,
+--    FOREIGN KEY (column_name) REFERENCES <NAME_OF_TABLE1>(column_name) ON DELETE CASCADE
+-- )
+CREATE TABLE players
+(
+  player_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  player_name VARCHAR(100) NOT NULL,
+  nationality VARCHAR(30) NOT NULL,
+  team_id INT UNSIGNED,
+  PRIMARY KEY (player_id),
+  FOREIGN KEY (team_id) REFERENCES teams(team_id) ON DELETE CASCADE
+);
